dd2f70c7af0743ae3e5fdfbe850e4ea063968fd2
dloh2236 <94659651+dloh2236@users.noreply.github.com>
2026-02-23 04:21:54 +0800

    Add db unit test

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -6,11 +6,11 @@ export default {
   testEnvironment: "node",
 
   // which test to run
-  testMatch: ["<rootDir>/controllers/*.test.js", "<rootDir>/helpers/*.test.js", "<rootDir>/middlewares/*.test.js", "<rootDir>/models/*.test.js"],
+  testMatch: ["<rootDir>/controllers/*.test.js", "<rootDir>/helpers/*.test.js", "<rootDir>/middlewares/*.test.js", "<rootDir>/models/*.test.js", "<rootDir>/config/*.test.js"],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["controllers/**", "helpers/**", "middlewares/**", "models/**"],
+  collectCoverageFrom: ["controllers/**", "helpers/**", "middlewares/**", "models/**", "config/**"],
   coverageThreshold: {
     global: {
       lines: 100,
