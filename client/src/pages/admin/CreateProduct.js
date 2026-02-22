6c54917ae488dba8029910584775e091ef50a053
danielleloh <szehan.loh@gmail.com>
2026-02-22 22:57:04 +0800

    test: add unit test for CreateProduct

--- client/src/pages/admin/CreateProduct.js
+++ client/src/pages/admin/CreateProduct.js
@@ -47,10 +47,12 @@ const CreateProduct = () => {
       productData.append("quantity", quantity);
       productData.append("photo", photo);
       productData.append("category", category);
+
       const { data } = await axios.post(
         "/api/v1/product/create-product",
         productData
       );
+      
       if (data?.success) {
         toast.success("Product Created Successfully");
         navigate("/dashboard/admin/products");
