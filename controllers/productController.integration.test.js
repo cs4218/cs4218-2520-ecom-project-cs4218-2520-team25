5ed3bd195253f1e303251b446bbd6f9a2c34e1a5
danielleloh <szehan.loh@gmail.com>
2026-03-20 17:11:27 +0800

    test: add integration test for updateProductController-productModel

--- controllers/productController.integration.test.js
+++ controllers/productController.integration.test.js
@@ -8,7 +8,8 @@ import {
   deleteProductController, 
   getProductController, 
   getSingleProductController, 
-  productPhotoController
+  productPhotoController,
+  updateProductController
 } from "./productController";
 import { describe } from "node:test";
 
@@ -674,7 +675,7 @@ describe("productPhotoController Integration Test (with productModel)", () => {
 });
 
 // Danielle Loh, A0257220N
-const seedProduct = async () => {
+const seedProductForDeletion = async () => {
   const category = await categoryModel.create({
     name: "Electronics",
     slug: "electronics",
@@ -691,11 +692,11 @@ const seedProduct = async () => {
   });
 
   return product;
-}
+};
 
 describe("deleteProductController Integration Test (with productModel)", () => {
   test("should delete existing product seccessfully", async () => {
-    const product = await seedProduct();
+    const product = await seedProductForDeletion();
 
     const req = {
       params: { pid: product._id },
@@ -740,7 +741,7 @@ describe("deleteProductController Integration Test (with productModel)", () => {
   });
 
   test("should handle database errors", async () => {
-    const product = await seedProduct();
+    const product = await seedProductForDeletion();
 
     await mongoose.disconnect();
 
@@ -754,6 +755,379 @@ describe("deleteProductController Integration Test (with productModel)", () => {
     expect(res.status).toHaveBeenCalledWith(500);
     const data = res.send.mock.calls[0][0];
     expect(data.success).toBe(false);
+
+    await mongoose.connect(mongoServer.getUri());
+  });
+});
+
+// Danielle Loh, A0257220N
+const seedProductForUpdate = async () => {
+  const category = await categoryModel.create({
+    name: "Electronics",
+    slug: "electronics",
+  });
+
+  const product = await productModel.create({
+    name: "Old Product",
+    slug: "old-product",
+    description: "Old product description",
+    price: 100,
+    category: category._id,
+    quantity: 1000,
+    shipping: true,
+  });
+
+  return { product, category };
+};
+
+describe("updateProductController Integration Test (with productModel)", () => {
+  test("should update existing product successfully (without photo)", async () => {
+    const { product, category } = await seedProductForUpdate();
+
+    const newCategory = await categoryModel.create({
+      name: "New Category",
+      slug: "new-category",
+    }); 
+    const req = {
+      params: { pid: product._id },
+      fields: {
+        name: "New Product",
+        description: "New product description",
+        price: 200,
+        category: newCategory._id,
+        quantity: 2000,
+        shipping: false,
+      },
+      files: {},
+    };
+    const res = mockResponse();
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(201);
+    const data = res.send.mock.calls[0][0];
+    expect(data.success).toBe(true);
+
+    // database check
+    const updatedProduct = await productModel.findById(product._id);
+    expect(updatedProduct.name).toBe("New Product");
+    expect(updatedProduct.slug).toBe(slugify("New Product"));
+    expect(updatedProduct.description).toBe("New product description");
+    expect(updatedProduct.price).toBe(200);
+    expect(updatedProduct.category.toString()).toBe(newCategory._id.toString());
+    expect(updatedProduct.quantity).toBe(2000);
+    expect(updatedProduct.shipping).toBe(false);
+  });
+
+  test("should update existing product with photo", async () => {
+    const { product, category } = await seedProductForUpdate();
+
+    const newCategory = await categoryModel.create({
+      name: "New Category",
+      slug: "new-category",
+    }); 
+    const req = {
+      params: { pid: product._id },
+      fields: {
+        name: "New Product",
+        description: "New product description",
+        price: 200,
+        category: newCategory._id,
+        quantity: 2000,
+        shipping: false,
+      },
+      files: {
+        photo: {
+          path: __filename,
+          type: "image/jpeg",
+          size: 500,
+        },
+      },
+    };
+    const res = mockResponse();
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(201);
+    const data = res.send.mock.calls[0][0];
+    expect(data.success).toBe(true);
+
+    const updatedProduct = await productModel.findById(product._id);
+    expect(updatedProduct.photo.data).toBeDefined();
+    expect(updatedProduct.photo.contentType).toBe("image/jpeg");
+  });
+
+  test("should not update if name is missing", async () => {
+    const { product, category } = await seedProductForUpdate();
+
+    const newCategory = await categoryModel.create({
+      name: "New Category",
+      slug: "new-category",
+    }); 
+    const req = {
+      params: { pid: product._id },
+      fields: {
+        description: "New product description",
+        price: 200,
+        category: newCategory._id,
+        quantity: 2000,
+        shipping: false,
+      },
+      files: {},
+    };
+    const res = mockResponse();
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    const responseData = res.send.mock.calls[0][0];
+    expect(responseData.error).toBe("Name is Required");
+
+    // database check
+    const calledProduct = await productModel.findById(product._id);
+    expect(calledProduct.name).toBe("Old Product");
+    expect(calledProduct.slug).toBe("old-product");
+    expect(calledProduct.description).toBe("Old product description");
+    expect(calledProduct.price).toBe(100);
+    expect(calledProduct.category.toString()).toBe(category._id.toString());
+    expect(calledProduct.quantity).toBe(1000);
+    expect(calledProduct.shipping).toBe(true);
+  });
+
+  test("should not update if description is missing", async () => {
+    const { product, category } = await seedProductForUpdate();
+
+    const newCategory = await categoryModel.create({
+      name: "New Category",
+      slug: "new-category",
+    }); 
+    const req = {
+      params: { pid: product._id },
+      fields: {
+        name: "New Product",
+        price: 200,
+        category: newCategory._id,
+        quantity: 2000,
+        shipping: false,
+      },
+      files: {},
+    };
+    const res = mockResponse();
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    const responseData = res.send.mock.calls[0][0];
+    expect(responseData.error).toBe("Description is Required");
+
+    // database check
+    const calledProduct = await productModel.findById(product._id);
+    expect(calledProduct.name).toBe("Old Product");
+    expect(calledProduct.slug).toBe("old-product");
+    expect(calledProduct.description).toBe("Old product description");
+    expect(calledProduct.price).toBe(100);
+    expect(calledProduct.category.toString()).toBe(category._id.toString());
+    expect(calledProduct.quantity).toBe(1000);
+    expect(calledProduct.shipping).toBe(true);
+  });
+
+  test("should not update if price is missing", async () => {
+    const { product, category } = await seedProductForUpdate();
+
+    const newCategory = await categoryModel.create({
+      name: "New Category",
+      slug: "new-category",
+    }); 
+    const req = {
+      params: { pid: product._id },
+      fields: {
+        name: "New Product",
+        description: "New product description",
+        category: newCategory._id,
+        quantity: 2000,
+        shipping: false,
+      },
+      files: {},
+    };
+    const res = mockResponse();
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    const responseData = res.send.mock.calls[0][0];
+    expect(responseData.error).toBe("Price is Required");
+
+    // database check
+    const calledProduct = await productModel.findById(product._id);
+    expect(calledProduct.name).toBe("Old Product");
+    expect(calledProduct.slug).toBe("old-product");
+    expect(calledProduct.description).toBe("Old product description");
+    expect(calledProduct.price).toBe(100);
+    expect(calledProduct.category.toString()).toBe(category._id.toString());
+    expect(calledProduct.quantity).toBe(1000);
+    expect(calledProduct.shipping).toBe(true);
+  });
+
+  test("should not update if category is missing", async () => {
+    const { product, category } = await seedProductForUpdate();
+
+    const req = {
+      params: { pid: product._id },
+      fields: {
+        name: "New Product",
+        description: "New product description",
+        price: 200,
+        quantity: 2000,
+        shipping: false,
+      },
+      files: {},
+    };
+    const res = mockResponse();
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    const responseData = res.send.mock.calls[0][0];
+    expect(responseData.error).toBe("Category is Required");
+
+    // database check
+    const calledProduct = await productModel.findById(product._id);
+    expect(calledProduct.name).toBe("Old Product");
+    expect(calledProduct.slug).toBe("old-product");
+    expect(calledProduct.description).toBe("Old product description");
+    expect(calledProduct.price).toBe(100);
+    expect(calledProduct.category.toString()).toBe(category._id.toString());
+    expect(calledProduct.quantity).toBe(1000);
+    expect(calledProduct.shipping).toBe(true);
+  });
+
+  test("should not update if quantity is missing", async () => {
+    const { product, category } = await seedProductForUpdate();
+
+    const newCategory = await categoryModel.create({
+      name: "New Category",
+      slug: "new-category",
+    }); 
+    const req = {
+      params: { pid: product._id },
+      fields: {
+        name: "New Product",
+        description: "New product description",
+        category: newCategory._id,
+        price: 200,
+        shipping: false,
+      },
+      files: {},
+    };
+    const res = mockResponse();
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    const responseData = res.send.mock.calls[0][0];
+    expect(responseData.error).toBe("Quantity is Required");
+
+    // database check
+    const calledProduct = await productModel.findById(product._id);
+    expect(calledProduct.name).toBe("Old Product");
+    expect(calledProduct.slug).toBe("old-product");
+    expect(calledProduct.description).toBe("Old product description");
+    expect(calledProduct.price).toBe(100);
+    expect(calledProduct.category.toString()).toBe(category._id.toString());
+    expect(calledProduct.quantity).toBe(1000);
+    expect(calledProduct.shipping).toBe(true);
+  });
+
+  test("should note update if photo exceeds size limit", async () => {
+    const { product, category } = await seedProductForUpdate();
+
+    const newCategory = await categoryModel.create({
+      name: "New Category",
+      slug: "new-category",
+    }); 
+    const req = {
+      params: { pid: product._id },
+      fields: {
+        name: "New Product",
+        description: "New product description",
+        price: 200,
+        category: newCategory._id,
+        quantity: 2000,
+        shipping: false,
+      },
+      files: {
+        photo: {
+          path: __filename,
+          type: "image/jpeg",
+          size: 2000000,
+        },
+      },
+    };
+    const res = mockResponse();
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    const data = res.send.mock.calls[0][0];
+    expect(data.error).toBe("photo is Required and should be less then 1mb");
+
+    // database check
+    const calledProduct = await productModel.findById(product._id);
+    expect(calledProduct.name).toBe("Old Product");
+    expect(calledProduct.slug).toBe("old-product");
+    expect(calledProduct.description).toBe("Old product description");
+    expect(calledProduct.price).toBe(100);
+    expect(calledProduct.category.toString()).toBe(category._id.toString());
+    expect(calledProduct.quantity).toBe(1000);
+    expect(calledProduct.shipping).toBe(true);
+    expect(calledProduct.photo.data).toBeUndefined();
+  });
+
+  test("should handle invalid product id", async () => {
+    const req = {
+      params: { pid: "invalid-id" },
+      fields: {
+        name: "Test Product",
+        description: "Test product description",
+        price: 200,
+        category: "test-cat",
+        quantity: 2000,
+      },
+      files: {},
+    };
+    const res = mockResponse();
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    const responseData = res.send.mock.calls[0][0];
+    expect(responseData.success).toBe(false);
+  });
+
+  test("should handle database errors", async () => {
+    const { product, category } = await seedProductForUpdate();
+
+    await mongoose.disconnect();
+
+    const req = {
+      params: { pid: product._id },
+      fields: {
+        name: "New Product",
+        description: "New product description",
+        price: 200,
+        category: category._id,
+        quantity: 2000,
+        shipping: false,
+      },
+      files: {},
+    };
+    const res = mockResponse();
+
+    await updateProductController(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    const responseData = res.send.mock.calls[0][0];
+    expect(responseData.success).toBe(false);
 
     await mongoose.connect(mongoServer.getUri());
   });
