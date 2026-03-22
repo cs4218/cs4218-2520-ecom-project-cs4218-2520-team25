7ef70dd02f00f0c2bbfdd104db589e7cfde3ac1e
danielleloh <szehan.loh@gmail.com>
2026-03-22 23:47:25 +0800

    fix: fix frontend bugs

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
     <div className="footer">
         <h4 className="text-center">All Rights Reserved &copy; TestingComp</h4>
         <p className="text-center mt-3">
-        <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
-        <Link to="/policy">Privacy Policy</Link>
-      </p>
+          <Link to="/about">About</Link> | <Link to="/contact">Contact</Link> |
+          <Link to="/policy">Privacy Policy</Link>
+        </p>
     </div>
   );
 };
