3b901481e40ec8704dd089fe1e07409997d5d59f
danielleloh <szehan.loh@gmail.com>
2026-04-12 13:42:52 +0800

    fix: add more details to getProductTest.js load test

--- tests/load-test/getProductTest.js
+++ tests/load-test/getProductTest.js
@@ -4,12 +4,18 @@ import { check, sleep } from 'k6';
 // run K6_WEB_DASHBOARD_PERIOD=1s K6_WEB_DASHBOARD=true k6 run tests/load-test/productPhotoTest.js
 
 // Danielle Loh, A0257220N
-const TEST_PRODUCT_SLUG = 'the-law-of-contract-in-singapore'; // real product ID from database
+
+const TEST_PRODUCT_SLUGS = [
+  'the-law-of-contract-in-singapore',
+  'Novel',
+  'NUS-T-shirt',
+  'Smartphone'
+]; // real product ID from database
 
 export const options = {
   stages: [
-    { duration: '2m', target: 100 }, // ramp up
-    { duration: '4m', target: 100 }, // sustain
+    { duration: '2m', target: 200 }, // ramp up
+    { duration: '4m', target: 200 }, // sustain
     { duration: '3m', target: 0 }, // ramp down
   ],
   // define thresholds
@@ -20,10 +26,21 @@ export const options = {
 };
 
 export default () => {
-  const res = http.get(`http://localhost:6060/api/v1/product/get-product/${TEST_PRODUCT_SLUG}`);
+  const productSlug = TEST_PRODUCT_SLUGS[Math.floor(Math.random() * TEST_PRODUCT_SLUGS.length)];
+  
+  const res = http.get(
+    `http://localhost:6060/api/v1/product/get-product/${productSlug}`
+  );
 
   check(res, {
     'status is 200 or 304': (r) => r.status === 200 || r.status === 304,
+    'response is successful': (r) => {
+      try {
+        return JSON.parse(r.body).success === true;
+      } catch {
+        return false;
+      }
+    },
   });
   
   sleep(Math.random() * 2 + 1); // think time: 1 – 3s between requests
